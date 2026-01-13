4f51463fdf1aebfbfa1cb3f5ad2cf05356d1c9fe
youngeui <iue_00@naver.com>
2026-01-13 21:56:14 +0900

    feat: 플리 페이지 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import MyPage from "./pages/profile/MyPage";
 import AiCreatePage from "./pages/ai/AICreatePage";
 import PlaylistPage from "./pages/song/PlaylistPage";
 import MyPlaylistPage from "./pages/profile/MyPlaylistPage";
+import MyPlaylistPersonal from "./pages/profile/MyPlaylistPersonal";
+import MyPlaylistLiked from "./pages/profile/MyPlaylistLiked";
 import SearchPage from "./pages/search/SearchPage";
 
 import ChartTop100 from "./pages/chart/ChartTop100";
@@ -31,7 +33,10 @@ export default function App() {
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/ai" element={<AiCreatePage />} />
         <Route path="/playlist/:playlistId" element={<PlaylistPage />} />
-        <Route path="/my-playlists" element={<MyPlaylistPage />} />
+        <Route path="/my-playlists" element={<MyPlaylistPage />}>
+        <Route path="personal" element={<MyPlaylistPersonal />} />
+        <Route path="liked" element={<MyPlaylistLiked />} />
+      </Route>
         <Route path="/search" element={<SearchPage />} />
       </Route>
 
